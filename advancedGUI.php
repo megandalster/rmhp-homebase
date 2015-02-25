f01b16c8554d818c129b1d8c0c08ab14d7aa07da
allen <allen@bowdoin.edu>
2015-02-25 07:39:35 -0800

    added example datepicker script

---
+++ advancedGUI.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<link rel="stylesheet" href="lib/jquery-ui.css" />
+<link rel="stylesheet" href="styles.css" type="text/css" />
+<script src="lib/jquery-1.9.1.js"></script>
+<script src="lib/jquery-ui.js"></script>
+<script>
+$(function() {
+	$( "#from" ).datepicker({dateFormat: 'yy-mm-dd',changeMonth:true,changeYear:true});
+	$( "#to" ).datepicker({dateFormat: 'yy-mm-dd',changeMonth:true,changeYear:true});
+});
+</script>
+</head>
+<body>
+<div>
+	<p>
+	<form method="post">
+		<p> <b>Example Datepicker Elements</b>
+		<br> Today's date: <?php echo Date("F d, Y");?></p>
+	<table>	<tr>
+		<td valign="top"> Select Date Range: 
+			<p> from : <input name = "from" type="text" id="from">
+							to : <input name = "to" type="text" id="to"></p>
+		</td>
+	</tr></table>
+	</form>
+</div>
+
+</body>
+</html>
+
+
